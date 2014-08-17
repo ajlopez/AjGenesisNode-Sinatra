310879be6784ab9abf7166e78d6d883daa6a4b33
ajlopez <webmaster@ajlopez.com>
2014-08-17 18:25:29 -0300

    Reference in model, WIP

--- source/ajgenesis/libs/models.js
+++ source/ajgenesis/libs/models.js
@@ -1,7 +1,11 @@
 
 var utils = require('./utils');
 
-function completeProperty(property) {
+function getEntityByName(entities, name) {
+    return utils.find(entities, 'name', name);
+}
+
+function completeProperty(property, entities) {
     if (!property)
         return;
         
@@ -9,28 +13,39 @@ function completeProperty(property) {
         property.title = utils.capitalize(property.name);
         
     if (!property.type)
-        property.type = 'text';
+        property.type = 'string';
+        
+    if (property.reference) {
+        property.type = 'reference';
+        property.reference = getEntityByName(entities, property.reference);
+    }
 }
 
-function completeEntity(entity) {
+function completeEntity(entity, entities) {
     if (!entity)
         return;
-        
-    if (entity.name && !entity.title)
-        entity.title = utils.capitalize(entity.name);
-
-    if (entity.name && !entity.classname)
-        entity.classname = utils.capitalize(entity.name);
         
     if (entity.name && !entity.setname)
         entity.setname = utils.pluralize(entity.name);
         
+    if (entity.name && !entity.title)
+        entity.title = utils.capitalize(entity.name);
+        
+    if (entity.name && !entity.classname)
+        entity.classname = utils.capitalize(entity.name);
+        
     if (entity.title && !entity.settitle)
         entity.settitle = utils.pluralize(entity.title);
         
+    if (!entity.references)
+        entity.references = [];
+        
     if (entity.properties)
         entity.properties.forEach(function (property) {
-            completeProperty(property);
+            completeProperty(property, entities);
+            
+            if (property.reference && property.reference.name && entity.references.indexOf(property.reference) < 0)
+                entity.references.push(property.reference);
         });
 }
 
@@ -38,24 +53,25 @@ function completeModel(model) {
     if (!model)
         return;
         
-    if (model.name && !model.title)
-        model.title = utils.capitalize(model.name);
-        
     if (!model.builddir)
         model.builddir = '.';
+        
+    if (model.name && !model.title)
+        model.title = utils.capitalize(model.name);
         
     if (model.project && model.project.name && !model.project.title)
         model.project.title = utils.capitalize(model.project.name);
         
     if (!model.entities)
         model.entities = [];
-        
-    if (model.entities && Array.isArray(model.entities))
+  
+    if (Array.isArray(model.entities))
         model.entities.forEach(function (entity) {
-            completeEntity(entity);
+            completeEntity(entity, model.entities);
         });
 }
 
 module.exports = {
     completeModel: completeModel
 };
+
--- test/models.js
+++ test/models.js
@@ -58,8 +58,57 @@ exports['Complete model with entities and properties'] = function (test) {
     test.equal(model.entities[1].settitle, 'Suppliers');
     
     test.equal(model.entities[0].properties[0].title, 'Name');
-    test.equal(model.entities[0].properties[0].type, 'text');
+    test.equal(model.entities[0].properties[0].type, 'string');
     test.equal(model.entities[0].properties[1].title, 'Address');
-    test.equal(model.entities[0].properties[1].type, 'text');
+    test.equal(model.entities[0].properties[1].type, 'string');
 };
 
+exports['Complete model with reference'] = function (test) {
+    var model = { 
+        name: 'project',
+        
+        entities: [
+            { 
+                name: 'employee',
+                properties: [
+                    {
+                        name: 'name'
+                    },
+                    {
+                        name: 'department',
+                        reference: 'department'
+                    }
+                ]
+            },
+            { 
+                name: 'department'
+            }
+        ]
+    };
+    
+    models.completeModel(model);
+    
+    test.equal(model.title, 'Project');
+
+    test.equal(model.entities[0].title, 'Employee');
+    test.equal(model.entities[0].classname, 'Employee');
+    test.equal(model.entities[0].settitle, 'Employees');
+    test.equal(model.entities[1].title, 'Department');
+    test.equal(model.entities[1].settitle, 'Departments');
+    
+    test.equal(model.entities[0].properties[0].title, 'Name');
+    test.equal(model.entities[0].properties[0].type, 'string');
+    test.equal(model.entities[0].properties[1].title, 'Department');
+    test.equal(model.entities[0].properties[1].type, 'reference');
+    test.equal(model.entities[0].properties[1].reference.name, 'department');
+    test.equal(model.entities[0].properties[1].reference.title, 'Department');
+    test.equal(model.entities[0].properties[1].reference.classname, 'Department');
+    test.equal(model.entities[0].properties[1].reference.settitle, 'Departments');
+    
+    test.ok(model.entities[0].references);
+    test.equal(model.entities[0].references.length, 1);
+    test.strictEqual(model.entities[0].references[0], model.entities[1]);
+
+    test.ok(model.entities[1].references);
+    test.equal(model.entities[1].references.length, 0);
+};
